5df9f262feaa5831cc78031d861b0f481252a9a7
rfranca <rcosta@gmfsaneamento.com.br>
2019-09-20 15:57:11 -0300

    adicionando registros manualmente no banco.

--- prjHelloSpringBoot/src/main/java/br/com/franca/PrjHelloSpringBootApplication.java
+++ prjHelloSpringBoot/src/main/java/br/com/franca/PrjHelloSpringBootApplication.java
@@ -1,18 +1,14 @@
 package br.com.franca;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.stream.LongStream;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.WebApplicationInitializer;
 
 import br.com.franca.domain.Usuario;
 import br.com.franca.repositories.UsuarioRepository;
-
 
 @SpringBootApplication
 public class PrjHelloSpringBootApplication {
@@ -20,13 +16,18 @@ public class PrjHelloSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrjHelloSpringBootApplication.class, args);
 	}
-	
-	/*
-	 * @Bean CommandLineRunner init(UsuarioRepository repository) { return args -> {
-	 * repository.deleteAll(); LongStream.range(1, 11).mapToObj(i -> { Usuario u =
-	 * new Usuario(); u.setChave("user" + i); u.setSenha("password" + i); return u;
-	 * }).map(v -> repository.save(v)).forEach(System.out::println); }; }
-	 */
 
+	@Bean
+	CommandLineRunner init(UsuarioRepository repository) {
+		return args -> {
+			repository.deleteAll();
+			LongStream.range(1, 11).mapToObj(i -> {
+				Usuario u = new Usuario();
+				u.setChave("user" + i);
+				u.setSenha("password" + i);
+				return u;
+			}).map(v -> repository.save(v)).forEach(System.out::println);
+		};
+	}
 
 }
