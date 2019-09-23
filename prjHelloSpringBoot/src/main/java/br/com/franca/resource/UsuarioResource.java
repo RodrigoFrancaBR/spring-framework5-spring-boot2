23222b396adb4b3a9108580f62cfbd0c1f863e48
rfranca <rcosta@gmfsaneamento.com.br>
2019-09-23 11:23:40 -0300

    alinhando com o devHome;

--- prjHelloSpringBoot/src/main/java/br/com/franca/resource/UsuarioResource.java
+++ prjHelloSpringBoot/src/main/java/br/com/franca/resource/UsuarioResource.java
@@ -1,16 +1,22 @@
 package br.com.franca.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.franca.domain.Usuario;
 import br.com.franca.service.UsuarioService;
+import br.com.franca.service.exception.ObjectNotFoundException;
 
 @RestController
 @RequestMapping({ "/usuarios" })
@@ -24,10 +30,23 @@ public class UsuarioResource {
 		return this.service.buscar();
 	}
 
-	@SuppressWarnings("rawtypes")
-	@GetMapping(path = {"/{id}"})
-	public ResponseEntity buscar(@PathVariable Integer id) {
-		return service.buscar(id);
+	@GetMapping(path = { "/{id}" })
+	public ResponseEntity<Usuario> buscar(@PathVariable Integer id) throws ObjectNotFoundException {
+		Usuario usuario = service.buscar(id);
+		return ResponseEntity.ok().body(usuario);
 	}
 
+	@PostMapping
+	public ResponseEntity<Void> salvar(@RequestBody Usuario usuario) {
+		Usuario resultado = service.salvar(usuario);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(resultado.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> alterar(@RequestBody Usuario usuario, @PathVariable Integer id ) throws ObjectNotFoundException {
+		service.alterar(usuario);
+		return ResponseEntity.noContent().build();
+	}
 }
--- prjHelloSpringBoot/src/main/java/br/com/franca/service/UsuarioService.java
+++ prjHelloSpringBoot/src/main/java/br/com/franca/service/UsuarioService.java
@@ -1,13 +1,16 @@
 package br.com.franca.service;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.franca.domain.Usuario;
 import br.com.franca.repositories.UsuarioRepository;
+import br.com.franca.service.exception.ObjectNotFoundException;
 
 @Service
 public class UsuarioService {
@@ -16,19 +19,27 @@ public class UsuarioService {
 	UsuarioRepository repository;
 
 	public List<Usuario> buscar() {
-		
+
 		// validações de negocios
-		
+
 		return repository.findAll();
 	}
 
-	@SuppressWarnings("rawtypes")
-	public ResponseEntity buscar(Integer id) {		
-		
-		// validações de negocios
-		
-		return repository.findById(id).map(resultado -> ResponseEntity.ok().body(resultado))
-				.orElse(ResponseEntity.notFound().build());
+	public Usuario buscar(Integer id) throws ObjectNotFoundException {
+		// validações de negocios		
+		Optional<Usuario> usuario = repository.findById(id);
+		return usuario.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo:" + Usuario.class.getName()));
 	}
 
+	public Usuario salvar(Usuario usuario) {
+		// usuario.setId(null);
+		return repository.save(usuario);
+		 
+	}
+
+	public Usuario alterar(Usuario usuario) throws ObjectNotFoundException {
+		buscar(usuario.getId());
+		return repository.save(usuario);		
+	}
 }
